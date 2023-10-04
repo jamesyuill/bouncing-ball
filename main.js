359fef4ab0a614b81ee5dfcd76ea7498b8aa7329
James Yuill <jamesyuill@jamess-mbp.lan>
2023-10-04 21:30:39 +0100

    removes init function and adds some buttons to start the audio context

--- main.js
+++ main.js
@@ -18,130 +18,138 @@ const camera = new THREE.PerspectiveCamera(
 // Audio Loader
 
 const startButton = document.getElementById('startButton');
-startButton.addEventListener('click', init);
+const removeAllButton = document.createElement('button');
+removeAllButton.innerText = 'Remove All Balls!';
 
-function init(e) {
-  e.stopPropagation();
-
-  const overlay = document.getElementById('overlay');
+const newButton = document.createElement('button');
+newButton.innerText = 'click to add ball';
+startButton.addEventListener('click', () => {
   Tone.start();
-  overlay.remove();
-
-  const gui = new GUI();
-  const effectsFolder = gui.addFolder(`Effects`);
-  effectsFolder.add(feedbackDelay.wet, 'value', 0.0, 0.5).name('Delay');
-  effectsFolder.add(shift.frequency, 'value', -200, 200).name('Shift');
-  effectsFolder.add(crusher.bits, 'value', 1, 16).name('Bit-Crush');
-  effectsFolder.open();
-
-  let ballCount = 1;
-  let spacer = -6;
-  let newMeshArray = [];
-  const newButton = document.createElement('button');
-  newButton.innerText = 'click to add ball';
-
-  newButton.addEventListener('click', () => {
-    const ballExp = new BallClass();
-    if (ballCount <= 1) {
-      ballExp.position.x = spacer;
-      ballExp.floor.position.x = spacer;
-    } else {
-      ballExp.position.x = spacer + ballCount;
-      ballExp.floor.position.x = spacer + ballCount;
-      spacer++;
-    }
-    newMeshArray.push(ballExp);
-
-    scene.add(ballExp);
-    const ballFolder = gui.addFolder(`Ball ${ballCount}`);
-    ballFolder.add(ballExp, 'acceleration', 100, 300);
-    ballFolder.add(ballExp, 'time_step', 0.01, 0.3);
-
-    ballFolder.open();
-    ballCount++;
-    if (ballCount >= 8) {
-      document.getElementById('add-button-div').removeChild(newButton);
-    }
-  });
-
+  document.getElementById('start-button-div').removeChild(startButton);
   document.getElementById('add-button-div').appendChild(newButton);
+  document.getElementById('add-button-div').appendChild(removeAllButton);
+});
+removeAllButton.addEventListener('click', removeAllBalls);
 
-  // Camera
+const gui = new GUI();
+const effectsFolder = gui.addFolder(`Effects`);
+effectsFolder.add(feedbackDelay.wet, 'value', 0.0, 0.5).name('Delay');
+effectsFolder.add(shift.frequency, 'value', -200, 200).name('Shift');
+effectsFolder.add(crusher.bits, 'value', 1, 16).name('Bit-Crush');
+effectsFolder.open();
 
-  camera.add(listener);
-  camera.lookAt(0.0, 0.0, 0.0);
-  camera.updateMatrixWorld();
+let ballCount = 1;
+let spacer = -6;
+let newMeshArray = [];
 
-  camera.position.z = 15;
-  camera.position.y = 15;
+newButton.addEventListener('click', () => {
+  const ballExp = new BallClass();
+  if (ballCount <= 1) {
+    ballExp.position.x = spacer;
+    ballExp.floor.position.x = spacer;
+  } else {
+    ballExp.position.x = spacer + ballCount;
+    ballExp.floor.position.x = spacer + ballCount;
+    spacer++;
+  }
+  newMeshArray.push(ballExp);
 
-  // Renderer
+  scene.add(ballExp);
+  const ballFolder = gui.addFolder(`Ball ${ballCount}`);
+  ballFolder.add(ballExp, 'acceleration', 100, 300);
+  ballFolder.add(ballExp, 'time_step', 0.01, 0.3);
 
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.setClearColor(0xffffff, 1);
-  renderer.shadowMap.enabled = true;
-  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-  document.body.appendChild(renderer.domElement);
+  ballFolder.open();
+  ballCount++;
+  if (ballCount >= 8) {
+    document.getElementById('add-button-div').removeChild(newButton);
+  }
+});
 
-  // Lighting
-  const light = new THREE.DirectionalLight(0xffffff, 3);
-  light.position.set(0, 9, 6);
-  // const helper = new THREE.DirectionalLightHelper(light, 10, 0xffffff);
-  light.castShadow = true;
-  scene.add(light);
-  light.shadow.mapSize.width = 1024; // default
-  light.shadow.mapSize.height = 1024; // default
-  light.shadow.camera.near = 0.1; // default
-  light.shadow.camera.far = 300;
-  light.shadow.camera.right = 20;
-  light.shadow.camera.left = -20;
-  light.shadow.camera.top = 20;
-  light.shadow.camera.bottom = -20;
+function removeAllBalls() {
+  renderer.renderLists.dispose();
+  // newMeshArray.forEach((obj) => {
+  //   obj.geometry.dispose();
+  //   obj.material.dispose();
+  //   scene.remove(obj);
+  // });
+}
 
-  // Orbit Controls
-  const controls = new OrbitControls(camera, renderer.domElement);
+// Camera
 
-  // Floor Mesh
-  const bigFloor = new FloorClass();
-  bigFloor.scale.x = 20;
-  bigFloor.scale.y = 10;
-  bigFloor.position.y = -0.01;
-  bigFloor.position.z = 4;
-  scene.add(bigFloor);
+camera.add(listener);
+camera.lookAt(0.0, 0.0, 0.0);
+camera.updateMatrixWorld();
 
-  // Animate Function
-  function animate() {
-    requestAnimationFrame(animate);
+camera.position.z = 15;
+camera.position.y = 15;
 
-    for (let i = 0; i < newMeshArray.length; i++) {
-      if (newMeshArray[i].position.y < newMeshArray[i].bottom_position_y) {
-        newMeshArray[i].time_counter = 0;
-      }
+// Renderer
 
-      newMeshArray[i].position.y =
-        newMeshArray[i].bottom_position_y +
-        newMeshArray[i].initial_speed * newMeshArray[i].time_counter -
-        0.5 *
-          newMeshArray[i].acceleration *
-          newMeshArray[i].time_counter *
-          newMeshArray[i].time_counter;
-      // advance time
-      newMeshArray[i].time_counter += newMeshArray[i].time_step;
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0xffffff, 1);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+document.body.appendChild(renderer.domElement);
 
-      if (newMeshArray[i].position.y === newMeshArray[i].bottom_position_y) {
-        newMeshArray[i].userData.isDown = true;
-        newMeshArray[i].bounce();
-        newMeshArray[i].changeFloorColor();
-      } else {
-        newMeshArray[i].userData.isDown = false;
+// Lighting
+const light = new THREE.DirectionalLight(0xffffff, 3);
+light.position.set(0, 9, 6);
+// const helper = new THREE.DirectionalLightHelper(light, 10, 0xffffff);
+light.castShadow = true;
+scene.add(light);
+light.shadow.mapSize.width = 1024; // default
+light.shadow.mapSize.height = 1024; // default
+light.shadow.camera.near = 0.1; // default
+light.shadow.camera.far = 300;
+light.shadow.camera.right = 20;
+light.shadow.camera.left = -20;
+light.shadow.camera.top = 20;
+light.shadow.camera.bottom = -20;
 
-        newMeshArray[i].changeFloorColor();
-      }
+// Orbit Controls
+const controls = new OrbitControls(camera, renderer.domElement);
+
+// Floor Mesh
+const bigFloor = new FloorClass();
+bigFloor.scale.x = 20;
+bigFloor.scale.y = 10;
+bigFloor.position.y = -0.01;
+bigFloor.position.z = 4;
+scene.add(bigFloor);
+
+// Animate Function
+function animate() {
+  requestAnimationFrame(animate);
+
+  for (let i = 0; i < newMeshArray.length; i++) {
+    if (newMeshArray[i].position.y < newMeshArray[i].bottom_position_y) {
+      newMeshArray[i].time_counter = 0;
     }
 
-    controls.update();
+    newMeshArray[i].position.y =
+      newMeshArray[i].bottom_position_y +
+      newMeshArray[i].initial_speed * newMeshArray[i].time_counter -
+      0.5 *
+        newMeshArray[i].acceleration *
+        newMeshArray[i].time_counter *
+        newMeshArray[i].time_counter;
+    // advance time
+    newMeshArray[i].time_counter += newMeshArray[i].time_step;
 
-    renderer.render(scene, camera);
+    if (newMeshArray[i].position.y === newMeshArray[i].bottom_position_y) {
+      newMeshArray[i].userData.isDown = true;
+      newMeshArray[i].bounce();
+      newMeshArray[i].changeFloorColor();
+    } else {
+      newMeshArray[i].userData.isDown = false;
+
+      newMeshArray[i].changeFloorColor();
+    }
   }
-  animate();
+
+  controls.update();
+
+  renderer.render(scene, camera);
 }
+animate();
